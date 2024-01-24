df47370214323e9edc13cff2b58a4ea1380e5716
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-01-24 15:41:13 +0100

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -2,15 +2,6 @@
 """
 This module defines the function minOperations, which calculates the minimum 
 number of operations required to obtain n 'H' characters in a file, using 
-
-
-
-
-i
-C
-C
-C
-
 only Copy All and Paste operations.
 """
 
@@ -19,11 +10,11 @@ def minOperations(n):
     Calculates the minimum number of operations to obtain exactly n 'H' characters.
 
     Args:
-    n (int): The desired number of 'H' characters.
+        n (int): The desired number of 'H' characters.
 
     Returns:
-    int: The minimum number of operations required to achieve n 'H' characters,
-         or 0 if n is not achievable.
+        int: The minimum number of operations required to achieve n 'H' characters,
+             or 0 if n is not achievable.
     """
     if n <= 1:
         return 0
