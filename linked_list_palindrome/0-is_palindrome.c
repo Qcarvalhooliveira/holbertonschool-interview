51788439da05f99f32f39cba8612f160aef1f293
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-10 17:38:48 +0100

    Fixing stdlib

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,12 +1,10 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
 * recurse_list - Compares 1st and last node, then 2nd last and 2nd, etc
-*
 * @left: Keeps track of left side of list
-*
 * @right: Keeps track of nodes on right
-*
 * Return: 0 if it is not a palindrome, 1 if it is a palindrome
 */
 
@@ -14,29 +12,30 @@ int recurse_list(listint_t **left, listint_t *right)
 {
 	int result;
 
-	if (right == NULL)
-	{
-		return (1);
+	if (right == NULL){
+		return 1;
 	}
 
 	result = recurse_list(left, right->next) && (*left)->n == right->n;
 
 	*left = (*left)->next;
 
-	return (result);
+	return result;
 }
 
 /**
 * is_palindrome - Checks if LL is a palindrome
-*
 * @head: Double pointer to head of list
-*
 * Return: 0 if it is not a palindrome, 1 if it is a palindrome
 */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *deref = *head;
+	listint_t *deref_head = *head;
 
-	return (recurse_list(&deref, deref));
+    if (!head || !*head) {
+        return 1; 
+    }
+
+    return recurse_list(&deref_head, deref_head);
 }
