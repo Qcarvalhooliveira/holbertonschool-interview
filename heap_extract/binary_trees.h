745945215bbee6d1809eb017692670df75cbfc84
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-05-31 15:46:35 +0200

    Create binary_trees.h

---
+++ heap_extract/binary_trees.h
@@ -0,0 +1,31 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
+
+int heap_extract(heap_t **root);
+heap_t *get_last_node(heap_t *root);
+size_t tree_height(const heap_t *tree);
+heap_t *find_last_node(heap_t *root, size_t height);
+void heapify_down(heap_t *node);
+
+#endif /* BINARY_TREES_H */
