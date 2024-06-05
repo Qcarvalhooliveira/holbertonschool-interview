24563592b4e03992a0ae0439d59462d68f74ca6f
Queise Carvalho <124299955+Qcarvalhooliveira@users.noreply.github.com>
2024-06-05 12:49:07 +0200

    Delete infinite_multiplication directory

--- infinite_multiplication/holberton.h
+++
@@ -1,9 +0,0 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-int _putchar(char c);
-void mul(char *num1, char *num2);
-int _strlen(char *str);
-int is_digit_string(char *str);
-
-#endif /* HOLBERTON_H */
