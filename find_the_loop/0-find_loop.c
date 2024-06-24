4354a5c0be335653d257efb190cdc05a738045fa
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-06-24 19:07:37 +0200

    Fixing betty style

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -4,32 +4,32 @@
  * find_listint_loop - finds the loop in a linked list
  * @head: pointer to the head of the list
  *
- * Return: The address of the node, or NULL if there is no loop
+ * Return: The address of the node where the loop starts, or NULL if there is no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow = head;
-    listint_t *fast = head;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-    if (!head)
-        return (NULL);
+	if (!head)
+		return (NULL);
 
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (slow == fast)
-        {
-            slow = head;
-            while (slow != fast)
-            {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return (slow);
-        }
-    }
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
 
-    return (NULL);
+	return (NULL);
 }
