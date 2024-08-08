0fd96c8d844b9d0e87f086d7a0bbe5441dd0e4b8
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-08-08 16:07:22 +0200

    fixing betty style

--- avl_trees/0-binary_tree_is_avl.c
+++ avl_trees/0-binary_tree_is_avl.c
@@ -42,7 +42,8 @@ int is_bst(const binary_tree_t *tree, int min, int max)
 		return (1);
 	if (tree->n <= min || tree->n >= max)
 		return (0);
-	return (is_bst(tree->left, min, tree->n) && is_bst(tree->right, tree->n, max));
+	return (is_bst(tree->left, min, tree->n) && 
+                    is_bst(tree->right, tree->n, max));
 }
 
 /**
