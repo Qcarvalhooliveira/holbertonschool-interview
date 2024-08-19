73db7f4fca521d6bf943f639114fa898ad3fc007
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-08-19 21:31:12 +0200

    change name

--- pascal_triangle/0-main.py
+++ pascal_triangle/0-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-0-main
+Main file for testing
 """
-pascal_triangle = __import__("0-pascal_triangle").pascal_triangle
+pascal_triangle = __import__("pascal_triangle").pascal_triangle
 
 def print_triangle(triangle):
     """
--- pascal_triangle/pascal_triangle.py
+++ pascal_triangle/pascal_triangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Module that defines the Pascal's triangle of n."""
 
 def pascal_triangle(n):
