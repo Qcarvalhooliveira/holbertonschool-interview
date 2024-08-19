84d60aaee0830f1d64f7d84ab094a86f265befd3
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-08-19 22:06:14 +0200

    Update 0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,56 @@
 #!/usr/bin/python3
-"""
-Script that draw a pascal triangle with a number of rows passed to the function
-"""
 
+"""Module that defines the Pascal's triangle of n."""
 
 def pascal_triangle(n):
     """
-    Function that recieve number of rows of the triangle and return the
-    triangle elements
+    Generate Pascal's triangle up to the nth row (inclusive).
+    
+    Args:
+        n (int): The number of rows of Pascal's triangle to generate.
+    
+    Returns:
+        List[List[int]]: A list of lists of integers representing 
+        Pascal's triangle up to the nth row. If n <= 0, returns an empty list.
+    """#!/usr/bin/python3
+
+"""Module that defines the Pascal's triangle of n."""
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the nth row (inclusive).
+    
+    Args:
+        n (int): The number of rows of Pascal's triangle to generate.
+    
+    Returns:
+        List[List[int]]: A list of lists of integers representing 
+        Pascal's triangle up to the nth row. If n <= 0, returns an empty list.
     """
     if n <= 0:
         return []
 
-    pascal = []
-    for i in range(n):
-        row = [1] * (i + 1)
-        for j in range(1, i):
-            row[j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
-        pascal.append(row)
+    triangle = [[1]]
 
-    return pascal
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    
+    return triangle
+
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    
+    return triangle
