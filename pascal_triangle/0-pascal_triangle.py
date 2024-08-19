4e4ddbc1ab494ebf3a6bb7d9f0b0c5f817fed260
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-08-19 17:38:39 +0200

    Fixing documentation

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """
-0-pascal_triangle
+Module that defines the Pascal's triangle of n.
 """
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing 
-    the Pascal’s triangle of n
+    Generate Pascal's triangle up to the nth row (inclusive).
+    
+    Args:
+        n (int): The number of rows of Pascal's triangle to generate.
+    
+    Returns:
+        List[List[int]]: A list of lists of integers representing 
+        Pascal's triangle up to the nth row. If n <= 0, returns an empty list.
     """
     if n <= 0:
         return []
@@ -15,12 +21,9 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         row = [1]
-
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
-
         row.append(1)
-
         triangle.append(row)
     
     return triangle
