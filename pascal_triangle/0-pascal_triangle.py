b15b5a2cd50f1bb6bd02b19415dc90c10928582e
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-08-19 16:10:49 +0200

    Update 0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-"""This module defines the Pascal’s triangle of n"""
+"""
+This module defines the Pascal’s triangle of n
+"""
+
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    Returns a list of lists of integers representing 
+    the Pascal’s triangle of n
     """
     if n <= 0:
         return []
