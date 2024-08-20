5ea8cddd116780ac80972aca4a28c16ac9b7b753
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-08-20 12:09:28 +0200

    Fixing pascal triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,56 +1,18 @@
 #!/usr/bin/python3
-
-"""Module that defines the Pascal's triangle of n."""
-
-def pascal_triangle(n):
-    """
-    Generate Pascal's triangle up to the nth row (inclusive).
-    
-    Args:
-        n (int): The number of rows of Pascal's triangle to generate.
-    
-    Returns:
-        List[List[int]]: A list of lists of integers representing 
-        Pascal's triangle up to the nth row. If n <= 0, returns an empty list.
-    """#!/usr/bin/python3
-
-"""Module that defines the Pascal's triangle of n."""
+"""Module that defines the Pascal's tringle of n."""
 
 def pascal_triangle(n):
-    """
-    Generate Pascal's triangle up to the nth row (inclusive).
-    
-    Args:
-        n (int): The number of rows of Pascal's triangle to generate.
-    
-    Returns:
-        List[List[int]]: A list of lists of integers representing 
-        Pascal's triangle up to the nth row. If n <= 0, returns an empty list.
-    """
+    """Generate Pascal's triangle up to nth row (inclusive)."""
     if n <= 0:
         return []
-
+    
     triangle = [[1]]
 
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(triangle[i-1][i-j] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
-    
-    return triangle
 
-    if n <= 0:
-        return []
-
-    triangle = [[1]]
-
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
-    
     return triangle
